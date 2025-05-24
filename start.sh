81d1942d53444ce15387ad55c419c4e5e5b8a716
sammy <sammy@noreply.forgejo>
2025-05-25 02:36:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-set -e
+set -ex
 
-# Start D-Bus session daemon in background
 dbus-daemon --session --print-address --fork
-
-# Start Avahi daemon in background
 avahi-daemon --daemonize
 
-# Start Python server in background
-python dns_server.py &
-
-# Keep the container alive (wait forever)
-tail -f /dev/null
+# Run python and print errors
+exec python dns_server.py
